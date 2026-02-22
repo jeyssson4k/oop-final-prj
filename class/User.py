7fc15a450365d97cfd0f2c4b06f9f5f2b929df41
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2026-02-22 02:56:10 +0000

    adding some logic to user

--- class/User.py
+++ class/User.py
@@ -3,4 +3,12 @@ class User:
     def __init__(self):
         self.balance = 1e3
         self.plants = []
+        self.current_power = 0 # there is no plants yet 
+        self.max_power = 0 # there is no max kwh because there is no plants yet
+
+    def change_used_plant_capacity(id:int, capacity: int):
+        # select a plant by id and then set the new used_capacity
+        # check if the capacity exceeds the plant's max_capacity
+        # if yes then assign the plant's max_capacity 
+        pass
     
