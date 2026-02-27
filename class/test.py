da4bfab1688dcc4732e4e46c7d6f0ec74423e0a9
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2026-02-27 17:12:22 +0000

    making a unit test that allows to print the representation of the class

---
+++ class/test.py
@@ -0,0 +1,9 @@
+from Plants import Coalplant
+from PlantParams import CoalPlantParams
+
+
+print("Testing the integration between plants and params\n")
+
+plant0 = Coalplant(CoalPlantParams)
+
+print(plant0)
